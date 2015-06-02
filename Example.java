d8351c706f4704da96434cf9b6f5c5dc1c7ceb4f
Karan Mehta <appllefanetickz@gmail.com>
2015-06-02 08:30:54 -0700

    Setup initial File structure

--- Example.java
+++ Example.java
@@ -1,8 +1,12 @@
 
 import java.util.Scanner;
 import java.io.File;
+import java.util.ArrayList;
 public class Example{
 	
+
+ArrayList<String> file = new ArrayList<String>();
+ArrayList<Method> methods = new ArrayList<Method>();
 
  public static void main(String[] args) {
  	Scanner getFile = new Scanner(System.in);
@@ -19,16 +23,25 @@ public class Example{
 
 
  	public void readFile(String filename){
+
+
  		try{
  			Scanner readFile = new Scanner(new File(filename));
  			
  			while(readFile.hasNext()){
- 				System.out.print(readFile.next());
+ 				file.add(new String(readFile.next()));
  			}
+
+ 			System.out.println(file);
+ 			findMethods();
 
  		}catch (Exception e) {
  			
  		}
+ 	}
+
+ 	public void findMethods(){
+ 		
  	}
 
 
--- Method.java
+++ Method.java
@@ -1,11 +1,29 @@
 
-
-
+import java.util.Scanner;
+import java.io.File;
 public class Method{
 	
 
-	public Method(){
+	public static void main(String[] args) {
+		Scanner readFile = null;
+		String methodContents  = null;
+		try{
+ 			 readFile = new Scanner(new File("Method.txt"));
+ 			 
+ 		}catch (Exception e) {
+ 			e.printStackTrace();
+ 		}
 
+ 		while(readFile.hasNext()){
+ 				methodContents+=readFile.next();
+ 		}
+ 		new Method().processMethod(methodContents);
+	}
+
+
+
+	private void processMethod(String methodContents){
+			
 		
 	}
 
